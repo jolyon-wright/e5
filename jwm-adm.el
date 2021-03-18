866ac78e8dbdcc4fc90d368304f1cdd9f54a3a25
Jolyon Wright <jolyon.wright@gmail.com>
2021-03-18 17:37:29 +0000

    ad ; initial commit

---
+++ jwm-adm.el
@@ -0,0 +1,5 @@
+;; https://github.com/woofers/org-invoice-template
+;; see also
+;; http://doc.norang.ca/org-mode.html#BillingClientsForClockedTime
+
+(straight-use-package '(woofers :type git :host github :repo "woofers/org-invoice-template"))
