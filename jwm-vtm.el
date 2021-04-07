490e6b56c25f5bd84109e36cfbe709ce25fe5e6a
Jolyon Wright <jolyon.wright@gmail.com>
2021-04-07 18:23:01 +0100

    vtm ; multi-vterm

--- jwm-vtm.el
+++ jwm-vtm.el
@@ -1,32 +1,16 @@
 
 (if (eq system-type 'windows-nt)
-    (message "vterm is broken on wondows - revisit")
-                                        ;(straight-use-package 'vterm)
+    (message "vterm is broken on wondows - revisit"))
+
+(when (not (equal window-system 'w32))
   (use-package vterm
     :defer t
     :init
     (setq vterm-always-compile-module t)
     )
-  )
-;; on mac:-
-; brew install libvterm
+  ;; on mac:-
+  ;; brew install libvterm
+  (use-package multi-vterm )
+  (bind-key "<C-f9>" 'multi-vterm))
 
 
-(when (not (equal window-system 'w32))
-
-  (defun jw-new-vshell ()
-    (interactive)
-    (let (
-          (currentbuf (get-buffer-window (current-buffer)))
-          (newbuf     (generate-new-buffer-name "*vterm*"))
-          )
-      (generate-new-buffer newbuf)
-      (set-window-dedicated-p currentbuf nil)
-      (set-window-buffer currentbuf newbuf)
-      (vterm newbuf)
-      )
-    )
-
-  ;; (bind-key "<C-f9>" #'vterm)
-  (bind-key "<C-f9>" 'jw-new-vshell)
-  )
