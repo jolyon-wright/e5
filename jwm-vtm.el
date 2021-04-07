196471ae9271bc9b4d56d9b6229127a6b17921d1
Jolyon Wright <jolyon.wright@gmail.com>
2021-04-07 16:52:57 +0100

    vtm ; allows multiple vterms

--- jwm-vtm.el
+++ jwm-vtm.el
@@ -11,6 +11,22 @@
 ;; on mac:-
 ; brew install libvterm
 
+
 (when (not (equal window-system 'w32))
-  (bind-key "<C-f9>" #'vterm)
+
+  (defun jw-new-vshell ()
+    (interactive)
+    (let (
+          (currentbuf (get-buffer-window (current-buffer)))
+          (newbuf     (generate-new-buffer-name "*vterm*"))
+          )
+      (generate-new-buffer newbuf)
+      (set-window-dedicated-p currentbuf nil)
+      (set-window-buffer currentbuf newbuf)
+      (vterm newbuf)
+      )
+    )
+
+  ;; (bind-key "<C-f9>" #'vterm)
+  (bind-key "<C-f9>" 'jw-new-vshell)
   )
