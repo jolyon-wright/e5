ff1f7ef31f26699680bb45f76dc9dd64e315e372
Jolyon Wright <jolyon.wright@gmail.com>
2021-04-13 06:55:27 +0100

    scl ; note added

--- jwm-scl.el
+++ jwm-scl.el
@@ -28,3 +28,4 @@
     ;; brew install mit-scheme
     (setq scheme-program-name   "/usr/local/bin/mit-scheme")
 )
+;; https://ocw.mit.edu/courses/electrical-engineering-and-computer-science/6-001-structure-and-interpretation-of-computer-programs-spring-2005/video-lectures/
