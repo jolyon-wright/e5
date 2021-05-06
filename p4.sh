dfffd4e1c71f95b8910bec23700f9dd6f7bc2086
Jolyon Wright <jolyon.wright@abaco.com>
2021-05-06 10:38:43 +0100

    p4 ; busted

--- p4.sh
+++ p4.sh
@@ -1,12 +1,21 @@
-#! /usr/bin/bash
+#!/bin/bash
+
+#https://www.perforce.com/downloads/helix-visual-client-p4v
+cat << EOF >> ~/.bashrc
+export PATH=$PATH:~/p4v-2021.1.2085655/bin/p4v
+
+EOF
+
+
 cd ~
 mkdir p4
-cd p4
+pushd p4
 wget https://package.perforce.com/perforce.pubkey
 gpg --with-fingerprint perforce.pubkey
 wget -qO - https://package.perforce.com/perforce.pubkey | sudo apt-key add -
+popd
 sudo cp ./perforce.list  /etc/apt/sources.list.d/perforce.list
 sudo apt-get update
 sudo apt-get install helix-p4d
 
-sudo /opt/perforce/sbin/configure-helix-p4d.sh
+#sudo /opt/perforce/sbin/configure-helix-p4d.sh
