007880e58fbb7e82d58bdfe0d3db2e75fd603331
Jolyon Wright <jolyon.wright@gmail.com>
2021-05-23 09:05:45 +0100

    dev ; projectile

--- jwm-dev.el
+++ jwm-dev.el
@@ -92,8 +92,7 @@
 
 
 (use-package projectile)
-;; (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;; (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (projectile-mode +1)
 
 (use-package flycheck)
