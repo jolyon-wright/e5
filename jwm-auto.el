87109c7c65456918a65ade640a3f6529f778c542
Jolyon Wright <jolyon.wright@gmail.com>
2021-10-31 13:06:20 +0000

    cu mode

--- jwm-auto.el
+++ jwm-auto.el
@@ -59,6 +59,7 @@
   (define-auto-insert "\\.h$" ["default-h" autoinsert-yas-expand])
   (define-auto-insert "\\.c$" ["default-c" autoinsert-yas-expand])
   (define-auto-insert "\\.py$" ["default-py" autoinsert-yas-expand])
+  (define-auto-insert "\\.cU$" ["default-cpp" autoinsert-yas-expand])
   (define-auto-insert "\\.cpp$" ["default-cpp" autoinsert-yas-expand]))
 
 
