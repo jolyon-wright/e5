7c755058fd05365f056e0b45d3d1beb0a20c26d5
Jolyon Wright <jolyon.wright@gmail.com>
2021-11-24 14:39:15 +0000

    flf ; i do not get on with vertico

--- jwm-fluff.el
+++ jwm-fluff.el
@@ -292,31 +292,31 @@
 (global-prettify-symbols-mode t)
 
 
-(use-package vertico
-  ;; :ensure t
-  ;; :bind (:map vertico-map
-  ;;        ("C-j" . vertico-next)
-  ;;        ("C-k" . vertico-previous)
-  ;;        ("C-f" . vertico-exit)
-  ;;        :map minibuffer-local-map
-  ;;        ("M-h" . backward-kill-word)
-  ;;        )
-  :custom
-  (vertico-cycle t)
-  :init
-  (vertico-mode))
+;; (use-package vertico
+;;   ;; :ensure t
+;;   ;; :bind (:map vertico-map
+;;   ;;        ("C-j" . vertico-next)
+;;   ;;        ("C-k" . vertico-previous)
+;;   ;;        ("C-f" . vertico-exit)
+;;   ;;        :map minibuffer-local-map
+;;   ;;        ("M-h" . backward-kill-word)
+;;   ;;        )
+;;   :custom
+;;   (vertico-cycle t)
+;;   :init
+;;   (vertico-mode))
 
-(use-package savehist
-  :init
-  (savehist-mode))
+;; (use-package savehist
+;;   :init
+;;   (savehist-mode))
 
-(use-package marginalia
-  :after vertico
-  ;; :ensure t
-  :custom
-  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
-  :init
-  (marginalia-mode))
+;; (use-package marginalia
+;;   :after vertico
+;;   ;; :ensure t
+;;   :custom
+;;   (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+;;   :init
+;;   (marginalia-mode))
 
 ;; (use-package orderless
 ;;   ;; :ensure t
