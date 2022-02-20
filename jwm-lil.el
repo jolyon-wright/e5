2de6fd6ac185656a2ee6d0ddf8a1f4d26313c629
Jolyon Wright <jolyon.wright@gmail.com>
2022-02-20 14:26:27 +0000

    lil ; initial

---
+++ jwm-lil.el
@@ -0,0 +1,48 @@
+;;; jwm-lil.el --- Description    -*- lexical-binding: t; -*-
+
+;; Author: Jolyon Wright  jolyon@Jolyons-MacBook-Pro.local
+;; Keywords:
+;; URL:
+
+;; Copyright (C) 2022, Jolyon Wright, all rights reserved.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(use-package lilypond-init
+  :straight nil
+  :load-path "/usr/local/Cellar/lilypond/2.22.1_1/share/emacs/site-lisp/lilypond"
+  :config
+  (setq org-babel-lilypond-arrange-mode t
+        org-babel-lilypond-commands '("lilypond" "timidity" "timidity")
+        org-babel-lilypond-gen-pdf nil
+        org-babel-lilypond-display-pdf-post-tangle nil)
+  :mode ("\\.ly\\'" . LilyPond-mode))
+
+(org-babel-do-load-languages 'org-babel-load-languages
+                             (append org-babel-load-languages
+                                     '(lilypond t)))
+
+(provide 'jwm-lil)
+
+;; Local Variables:
+;; coding: utf-8
+;; End:
+
+;;; jwm-lil.el ends here.
