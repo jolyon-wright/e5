0a810d4aa0340eeb2a2457991291e04b538a7a3d
U-DESKTOP-BE6F7KR\jolyon <jolyon.wright@gmail.com>
2022-03-16 09:21:46 +0000

    more

--- file-finder/file-finder.cpp
+++ file-finder/file-finder.cpp
@@ -18,22 +18,83 @@
 
 #include <iostream>
 #include <filesystem>
+#include <condition_variable>
+#include <mutex>
 #include <thread>
 #include <vector>
+#include <queue>
 #include <string>
+#include <cassert>
+#include <shared_mutex>
 
 using namespace std;
 
+// types
+typedef std::queue<string>  string_queue;
+typedef std::vector<thread> active_thread_vct;
+
+
 typedef std::vector<string> string_vct;
 
-void thread_function();
+// void thread_function();
+
+struct substring_container
+{
+    bool         is_ready_;
+    string_queue container_;
+    shared_mutex container_mutex; // a read/write lock
+    // condition ?
+
+    substring_container()
+            : is_ready_(false)
+    {}
+};
+
+// globals (sorry)
+
+substring_container g_container;
 
 
-int
-scan_for_files(const char*       StartDir,
-               const string_vct& MatchVct
-               );
 
+// methods
+void
+substring_adder_provider_thread(const char* StartDir,
+                                const char* Pattern
+                                )
+{
+    assert(StartDir);
+    assert(Pattern);
+
+    // cout << "startdir:" << string(StartDir) << " pattern:" << string(Pattern) << endl;
+
+    // find all matches for this pattern and put them in the container
+    // but (!!!) be prepared to abort if the termination event is set!
+
+    try {
+        for (const auto& entry : filesystem::recursive_directory_iterator(StartDir)) {
+            // we only care about files
+            if (filesystem::is_regular_file(entry)) {
+                std::cout << "dir:" << entry << " (need to find " << Pattern << ")" << std::endl;
+                // we only want the filename (which is a path *not* a string!)
+                g_container.container_.push(entry.path().filename().string());
+            }
+
+            // time to terminate ?
+        }
+    }
+    catch (const filesystem::filesystem_error& exception) {
+        cerr << "ERROR : filesystem exception thrown:" << exception.what() << endl;
+    }
+}
+
+
+// int
+// scan_for_files(const char*       StartDir,
+//                const string_vct& MatchVct
+//                );
+
+
+// implementation
 int
 main(int    argc,
      char** argv
@@ -46,16 +107,32 @@ main(int    argc,
   int ret_val{-1}; // default to error
 
   try {
-    string_vct match_vct;
+      active_thread_vct active_thread;
 
-    while (--argc) {
-      match_vct.push_back(argv[argc]);
-    }
+      //string_vct match_vct;
 
-    ret_val = scan_for_files(argv[1],
-                             match_vct
-                             );
+      while (--argc > 1) {
+          active_thread.push_back(thread(substring_adder_provider_thread,
+                                         argv[1],   // this is the start directory
+                                         argv[argc] // this is the pattern
+                                         )
+                                  );
 
+          cout << "startdir:" << string(argv[1]) << " pattern:" << string(argv[argc]) << endl;
+          //      match_vct.push_back(argv[argc]);
+      }
+
+
+      // wait for all threads to terminate
+      for (std::thread& thd : active_thread) {
+          if (thd.joinable()) {
+              thd.join();
+          }
+      }
+
+      // ret_val = scan_for_files(argv[1],
+      //                          match_vct
+      //                          );
   }
   catch(const std::bad_alloc&) {
     cerr << "FATAL: Memory Allocation Failure" << endl;
@@ -87,19 +164,19 @@ scan_for_files(const char*       StartDir,
 
 
 
-  std::thread threadObj(thread_function);
-  threadObj.join();
+  // std::thread threadObj(thread_function);
+  // threadObj.join();
 
-  puts("im back");
+  // puts("im back");
 
 
   return ret_val;
 }
 
 
-void thread_function()
-{
-    for(int i = 0; i < 10000; i++);
+// void thread_function()
+// {
+//     for(int i = 0; i < 10000; i++);
 
-    std::cout<<"thread function Executing"<<std::endl;
-}
+//     std::cout<<"thread function Executing"<<std::endl;
+// }
