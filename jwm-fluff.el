a6948586b693ecd345545c8cc3d45f0e3cb25b0e
Jolyon Wright <jolyon.wright@gmail.com>
2022-04-22 22:02:00 +0100

    added register bindings

--- jwm-fluff.el
+++ jwm-fluff.el
@@ -395,8 +395,8 @@
          ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
          ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
          ;; Custom M-# bindings for fast register access
-         ("M-#" . consult-register-load)
-         ("M-'" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
+         ("C-s-v" . consult-register-load)
+         ("C-s-c" . consult-register-store)          ;; orig. abbrev-prefix-mark (unrelated)
          ("C-M-#" . consult-register)
          ;; Other custom bindings
          ("M-y" . consult-yank-pop)                ;; orig. yank-pop
